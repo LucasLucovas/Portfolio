b53e327a90f1e1d8c9678b39d537fe3a09ece698
Lucas Lucovas <lucaslucovas@gmail.com>
2024-03-29 00:09:30 -0300

    cambios en portfolio en el h2

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,11 @@
 import { Projects } from "./Projects"
-
+import { Typography } from "@mui/joy"
 
 
 const Portfolio = () => {
   return (
     <>
-    <h2>Personal Projects</h2>
+    <Typography variant='h2' fontSize={22} margin={2}>Personal Projects</Typography>
     <Projects/>
     <footer>©2023 Lucas Lucovas</footer>
   </>
